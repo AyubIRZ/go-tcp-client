bd8f7046f8b4f75c2a499df811f3a4fca63d1ac9
AyubIRZ <ayubirazeh@gmail.com>
2020-04-25 18:20:07 +0430

    add connecting to TCP server

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 )
 
+const TCPServer = "localhost:6060"
+
 func main() {
-	fmt.Println("It works!!!")
+	fmt.Println("* * * TCP client started * * *")
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", TCPServer)
+	if err != nil {
+		log.Fatal("TCP resolve failed: ", err)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		log.Fatal("TCP conn failed: ", err)
+	}
+	defer conn.Close()
+
+	fmt.Fprint(conn, "Hello from TCP client!!!\n")
 }
